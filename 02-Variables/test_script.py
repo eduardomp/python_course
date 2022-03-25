88a5b1c7fed5aa38c4a21b9935c1ce430acc9725
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2022-03-25 00:48:33 +0000

    better feedback to the student

--- 02-Variables/test_script.py
+++ 02-Variables/test_script.py
@@ -1,53 +1,71 @@
 def check_jeff(isJeffBezosRichestPerson):
-    assert type( isJeffBezosRichestPerson ) is bool
+    assert type( isJeffBezosRichestPerson ) is bool , "Wrong type!"
+    print("OK")
 
 def check_company(company):
-    assert type( company ) is str
+    assert type( company ) is str , "Wrong type!"
+    print("OK")
 
 def check_netWorth(netWorth):
-    assert type( netWorth ) is float
+    assert type( netWorth ) is float, "Wrong type!"
+    print("OK")
 
 def check_mostPopularWebsite(mostPopularWebsite):
-    assert type( mostPopularWebsite ) is str
+    assert type( mostPopularWebsite ) is str, "Wrong type!"
+    print("OK")
 
 def check_evenPrimeNumber(evenPrimeNumber):
-    assert type( evenPrimeNumber ) is int
+    assert type( evenPrimeNumber ) is int, "Wrong type!"
+    print("OK")
 
 def check_notRomanNumeral(notRomanNumeral): 
-    assert type( notRomanNumeral ) is int
+    assert type( notRomanNumeral ) is int, "Wrong type!"
+    print("OK")
 
 def check_seconds(seconds):
-    assert type( seconds ) is int
+    assert type( seconds ) is int, "Wrong type!"
+    print("OK")
 
 def check_language(language):
-    assert type( language ) is str
+    assert type( language ) is str, "Wrong type!"
+    print("OK")
 
 def check_popularLanguage(popularLanguage):
-    assert type( popularLanguage ) is str
+    assert type( popularLanguage ) is str, "Wrong type!"
+    print("OK")
 
 def check_horrorCharacter(horrorCharacter):
-    assert type( horrorCharacter ) is str
+    assert type( horrorCharacter ) is str, "Wrong type!"
+    print("OK")
 
 def check_mostsVisitedCountry(mostsVisitedCountry):
-    assert type( mostsVisitedCountry ) is str
+    assert type( mostsVisitedCountry ) is str, "Wrong type!"
+    print("OK")
 
 def check_nonMetricCount(nonMetricCount): 
-    assert type( nonMetricCount ) is str
+    assert type( nonMetricCount ) is str, "Wrong type!"
+    print("OK")
 
 def check_isUSMetric(isUSMetric):
-    assert type( isUSMetric ) is bool
+    assert type( isUSMetric ) is bool, "Wrong type!"
+    print("OK")
 
 def check_coldestTemp(coldestTemp):
-    assert type( coldestTemp ) is int
+    assert type( coldestTemp ) is int, "Wrong type!"
+    print("OK")
 
 def check_population(population):
-    assert type( population ) is float
+    assert type( population ) is float, "Wrong type!"
+    print("OK")
 
 def check_worldPopulation(worldPopulation):
-    assert type( worldPopulation ) is int
+    assert type( worldPopulation ) is int, "Wrong type!"
+    print("OK")
 
 def check_pi(pi):
-    assert type( pi ) is  float
+    assert type( pi ) is  float, "Wrong type!"
+    print("OK")
 
 def check_dataVolume(dataVolume):
-    assert type( dataVolume ) is int
+    assert type( dataVolume ) is int, "Wrong type!"
+    print("OK")
