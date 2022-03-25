3867febb9d26fce6eb12e004669cfda4c3eb7273
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2022-03-25 00:32:11 +0000

    second module in ipynb and tests

---
+++ 02-Variables/test_script.py
@@ -0,0 +1,53 @@
+def check_jeff(isJeffBezosRichestPerson):
+    assert type( isJeffBezosRichestPerson ) is bool
+
+def check_company(company):
+    assert type( company ) is str
+
+def check_netWorth(netWorth):
+    assert type( netWorth ) is float
+
+def check_mostPopularWebsite(mostPopularWebsite):
+    assert type( mostPopularWebsite ) is str
+
+def check_evenPrimeNumber(evenPrimeNumber):
+    assert type( evenPrimeNumber ) is int
+
+def check_notRomanNumeral(notRomanNumeral): 
+    assert type( notRomanNumeral ) is int
+
+def check_seconds(seconds):
+    assert type( seconds ) is int
+
+def check_language(language):
+    assert type( language ) is str
+
+def check_popularLanguage(popularLanguage):
+    assert type( popularLanguage ) is str
+
+def check_horrorCharacter(horrorCharacter):
+    assert type( horrorCharacter ) is str
+
+def check_mostsVisitedCountry(mostsVisitedCountry):
+    assert type( mostsVisitedCountry ) is str
+
+def check_nonMetricCount(nonMetricCount): 
+    assert type( nonMetricCount ) is str
+
+def check_isUSMetric(isUSMetric):
+    assert type( isUSMetric ) is bool
+
+def check_coldestTemp(coldestTemp):
+    assert type( coldestTemp ) is int
+
+def check_population(population):
+    assert type( population ) is float
+
+def check_worldPopulation(worldPopulation):
+    assert type( worldPopulation ) is int
+
+def check_pi(pi):
+    assert type( pi ) is  float
+
+def check_dataVolume(dataVolume):
+    assert type( dataVolume ) is int
